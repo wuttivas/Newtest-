f55c1d43ad2436af3b651e724a36028ed0e22c05
wuttivas <savittuw@hotmail.co.th>
2018-03-24 19:09:05 +0700

    Initial commit

---
+++ Assets/Scripts/Menuscript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class Menuscript
+{
+    [MenuItem("Tools/Assign Tile Material")]
+    public static void AssignTileMaterial()
+    {
+        GameObject[] tile = GameObject.FindGameObjectsWithTag("Tile");
+        Material material = Resources.Load<Material>("Tile");
+
+        foreach (GameObject t in tile)
+        {
+            t.GetComponent<Renderer>().material = material;
+        }
+    }
+
+    [MenuItem("Tools/Assign Tile Script")]
+    public static void AssignTileScript()
+    {
+        GameObject[] tile = GameObject.FindGameObjectsWithTag("Tile");
+
+        foreach (GameObject t in tile)
+        {
+            t.AddComponent<Tile>();
+        }
+    }
+
+}
