9751ae0497cc675df740c0bf89df2dfbaa3d638a
wuttivas <savittuw@hotmail.co.th>
2018-03-24 21:49:02 +0700

    เป็นอีกละ

--- Assets/Scripts/Playermove.cs
+++ Assets/Scripts/Playermove.cs
@@ -14,5 +14,33 @@ public class Playermove : TacticsMove {
 	void Update ()
     {
         FindSelectableTiles();
+        checkedMouse();
 	}
+    else
+    {
+    //todo: Move();
+    }
+}
+
+void checkMouse()
+{
+    if (Input.GetMouseButtonUp(0))
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+        {
+            if hit.collider.GetComponent<Tile>();
+
+            if (this.selectable)
+            {
+                //todo: move target
+                T.target = true;
+                moving = true;
+            }
+
+
+        }
+    }
 }
